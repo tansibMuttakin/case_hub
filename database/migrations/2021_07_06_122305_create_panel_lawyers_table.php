0913daca896682389bc4903622ffa2db28ea51d0
tansibMuttakin <tansib.muhaimin@northsouth.edu>
2021-07-06 18:35:30 +0600

    PanelLawyer model, migration controller created

---
+++ database/migrations/2021_07_06_122305_create_panel_lawyers_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePanelLawyersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('panel_lawyers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('mother_name');
+            $table->string('father_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('date_of_birth');
+            $table->string('nationality');
+            $table->string('religion');
+            $table->date('date_of_enrollment');
+            $table->string('bar_name');
+            $table->string('membership_number');
+            $table->text('chamber_address');
+            $table->text('residence_address');
+            $table->string('district_name');
+            $table->text('specialized_practice_area');
+            $table->string('experience');
+            $table->string('case_conducted');
+            $table->text('reference');
+            $table->text('remarks');
+            $table->string('status');
+            $table->string('lawyer_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('panel_lawyers');
+    }
+}
